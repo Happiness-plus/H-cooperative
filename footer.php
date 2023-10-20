1420cf7e4ae8f6b08838ee2eb95a58ece0e996c4
Wsettles <48694096+Happiness-plus@users.noreply.github.com>
2023-10-19 22:48:46 -0400

    we're almost done here
    Now I just fix more padding issues

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="text-center py-4 align-items-center" >
+    <a data-aos="zoom-out-down" href="#contact" style="color: #111;
+  background-color: #FFB4B4; " class="btn btn-dark rounded-pill learn-more">Contact Us</a>
+
+        <a href="terms.html">Terms & Conditions</a>
+        <br>
+        <a href="privacy.html">Privacy Policy</a>
+        <br>
+        <p>2023 Hentai Cooperative LLC</p>
+    </div>
+    </footer>
